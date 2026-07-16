3b86077e345d494ab3d95a08c52703dc5b657029
viethung0202 <laiviethung2003@gmail.com>
2026-07-16 17:44:11 +0700

    Hide sidebar when not logged in

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,13 +2,16 @@ import { Outlet } from 'react-router-dom'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import { Sidebar } from '@/components/Sidebar'
+import { useMe } from '@/hooks/useMe'
 
 export function MainLayout() {
+  const { data: user } = useMe()
+
   return (
     <div className="flex min-h-svh flex-col">
       <Header />
       <div className="flex flex-1">
-        <Sidebar />
+        {user && <Sidebar />}
         <main className="w-full flex-1 px-4 py-8">
           <Outlet />
         </main>
